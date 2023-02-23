104b0439acc0ffcd4a57a55efd0c6be796b74b15
Kursat Sahin <a.kursat.sahin@gmail.com>
2023-02-23 19:16:25 +0300

    basic home scene was implemented

---
+++ Assets/Scripts/Core/TextKeywords.cs
@@ -0,0 +1,11 @@
+﻿namespace Core
+{
+    public static class TextKeywords
+    {
+        public static class Home
+        {
+            public const string HomePlayButtonText = "PLAY";
+            public const string HomeExitButtonText = "EXIT";
+        }
+    }
+}
