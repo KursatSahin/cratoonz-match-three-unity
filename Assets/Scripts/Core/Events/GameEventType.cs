186984c5b9e0b1d58ef63bf356de3ffcfc736fab
Kursat Sahin <a.kursat.sahin@gmail.com>
2023-02-24 05:08:42 +0300

    EventDispatcher was added as a service

---
+++ Assets/Scripts/Core/Events/GameEventType.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Events
+{
+    public enum GameEventType : ushort
+    {
+        // TODO: Add events and remove following placeholders
+        event_1,
+        event_2,
+    }
+}
---
+++ Assets/Scripts/Core/Events/Interfaces/IEventDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using Core.Services;
+
+namespace Core.Events
+{
+    public delegate void EventAction(IEvent e);
+    
+    public interface IEventDispatcher : IService
+    {
+        void Subscribe(GameEventType gameEventType, EventAction listener);
+        void Unsubscribe(GameEventType gameEventType, EventAction listener);
+        void Fire(GameEventType gameEventType, IEvent e = null);
+    }
+}
