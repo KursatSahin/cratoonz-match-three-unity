9a33e367e28715febafbb476b7e4568b51bd34bf
Kursat Sahin <a.kursat.sahin@gmail.com>
2023-02-24 11:10:06 +0300

    BoardSettings scriptable object and CameraController were created
    CameraController adjust orthographic camera size with fixed tile size

---
+++ Assets/Scripts/Game/BoardSettingsSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = "CMatch/Board/BoardSettingsSO", fileName = nameof(BoardSettingsSO))]
+    public class BoardSettingsSO : ScriptableObject
+    {
+        [Header("BoardSettings")]
+        public int BoardWidth = 8;
+        public int BoardHeight = 8;
+    }
+}
---
+++ Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Game
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float _tileSize = 1.28f;
+        [SerializeField] private float _boardScreenPercent = 94.81f;
+        [SerializeField] private BoardSettingsSO _boardSettings;
+
+        private float BoardScreenRatio => _boardScreenPercent / 100;
+        
+        //private float boardWidth = 1080 * _boardScreenRatio / 100;
+        
+        /// <summary>
+        /// Adjust orthographic camera size with fixed tile size value
+        /// </summary>
+        private void AdjustOrthographicCameraSize()
+        {
+            float boardWidthInWorldSpace = _tileSize * _boardSettings.BoardWidth;
+            float calculatedScreenWidthInWorldSpace = boardWidthInWorldSpace * 100;
+
+            float screenRatio = (float) Screen.width / Screen.height;
+            float targetRatio = (_boardSettings.BoardWidth * _tileSize) / (_boardSettings.BoardHeight * _tileSize); // 9/16 = 0.56
+
+            if (screenRatio >= targetRatio)
+            {
+                Camera.main.orthographicSize = ((_boardSettings.BoardHeight * _tileSize) / BoardScreenRatio) / 2;
+            }
+            else
+            {
+                float differenceInSize = targetRatio / screenRatio;
+                Camera.main.orthographicSize = ((_boardSettings.BoardHeight * _tileSize) / BoardScreenRatio) / 2 * differenceInSize;
+            }
+        }
+        private void Start()
+        {
+            AdjustOrthographicCameraSize();
+        }
+    }
+}
